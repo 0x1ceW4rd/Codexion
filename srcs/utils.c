c75672a47c34c83c939a095893f835b21b7005ec
3dnane-GM <mr.adnaneezzirari@gmail.com>
2026-09-09 17:09:38 +0100

    I am almost done

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: aezzirar <aezzirar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/09 13:42:51 by aezzirar          #+#    #+#             */
-/*   Updated: 2026/09/09 15:55:55 by aezzirar         ###   ########.fr       */
+/*   Updated: 2026/09/09 16:44:42 by aezzirar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,15 @@ int	is_sim_stopped(t_sim *sim)
 
 void	print_log(t_sim *sim, int coder_id, const char *msg)
 {
-	int			stopped;
 	long long	now;
 
-	pthread_mutex_lock(&sim->print_mutex);
 	pthread_mutex_lock(&sim->state_mutex);
-	stopped = sim->stop_flag;
-	pthread_mutex_unlock(&sim->state_mutex);
-	if (!stopped)
+	if (!sim->stop_flag)
 	{
+		pthread_mutex_lock(&sim->print_mutex);
 		now = get_sim_time(sim->start_time);
 		printf("%lld %d %s\n", now, coder_id, msg);
+		pthread_mutex_unlock(&sim->print_mutex);
 	}
-	pthread_mutex_unlock(&sim->print_mutex);
+	pthread_mutex_unlock(&sim->state_mutex);
 }
